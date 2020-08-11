ba390f4d008ef768b7bbdee5bc7751f9ef4608e7
d3npa <d3npa@pm.me>
2020-08-11 10:57:58 +0900

    Move static files

--- universe.py
+++ universe.py
@@ -4,12 +4,12 @@ from flask import Flask, send_from_directory, render_template, safe_join
 app = Flask(__name__)
 
 APP_ROOT = f'{os.path.dirname(__file__)}'
-CSS_ROOT = APP_ROOT + '/css'
+STATIC_ROOT = APP_ROOT + '/static'
 FILES_ROOT = APP_ROOT + '/files'
 
 @app.route('/css/<path:stylesheet>')
 def serve_css(stylesheet):
-	return send_from_directory('css', stylesheet)
+	return send_from_directory(f'{STATIC_ROOT}/css', stylesheet)
 
 @app.route('/')
 @app.route('/<path:file>')
