2dc904dcf666703288e311243ce176c447596ebd
d3npa <d3npa@pm.me>
2020-11-11 19:10:36 -0500

    fix dir listings and added cmd parsing

--- universe.py
+++ universe.py
@@ -1,10 +1,11 @@
 import os
+import re
 import shlex
 from sys import argv
 from datetime import datetime
 from subprocess import Popen, PIPE
 from flask import (
-	Flask, send_from_directory, render_template, safe_join, request
+	Flask, send_from_directory, render_template, safe_join, request, redirect
 )
 app = Flask(__name__)
 
@@ -23,7 +24,8 @@ def remote_addr():
 
 
 def exec_cmd(cmd):
-	process = Popen(shlex.split(cmd), stdout=PIPE, stderr=PIPE)
+	shell = shlex.split(f'sh -c \'{cmd}\'')
+	process = Popen(shell, stdout=PIPE, stderr=PIPE, env={'LC_ALL':'C'})
 	stdout, stderr = process.communicate()
 	return (stdout.decode('utf-8'), stderr.decode('utf-8'))
 
@@ -45,14 +47,17 @@ def serve_directory(dirpath):
 		return serve_file(dirpath)
 	html = ''
 	os.chdir(dirpath)
-	for f in os.listdir(dirpath):
-		line, _ = exec_cmd(f'ls -l {f}')
-		html += f'<a href="./{f}">{line} -></a>'
+	listing, _ = exec_cmd(f'ls -ld *')
+	for line in listing.strip().split('\n'):
+		f = line.split(' ')[-1]
+		if os.path.isdir(f):
+			f += '/'
+		html += f'<a href="./{f}">{line.strip()} -></a><br>'
 	os.chdir('..')
 	return render_template(
 		'base.jinja',
 		title=dirpath, 
-		body=html, 
+		body=f'<pre>{html}</pre>', 
 		client_ip=remote_addr(),
 		url_up=url_up(),
 		)
@@ -62,7 +67,15 @@ def url_up():
 	path = request.path
 	if path[-1] == '/':
 		path = path[:-1]
-	return '/'.join(path.split('/')[:-1]) or '/'
+	return '/'.join(path.split('/')[:-1]) + '/' or '/'
+
+
+def parse_cmds(textdata):
+	cmds = re.findall('\$\((.+?)\)', textdata)
+	for cmd in cmds:
+		output = ''.join(exec_cmd(cmd))
+		textdata = textdata.replace(f'$({cmd})', f'<pre>{output}</pre>', 1)
+	return textdata
 
 
 @app.route('/')
@@ -72,14 +85,17 @@ def serve_file(file='index'):
 	if not os.path.exists(path):
 		return render_template('error404.jinja', title='404', path=file)
 	if os.path.isdir(path):
+		if file[-1] != '/':
+			return redirect(file + '/')
 		return serve_directory(path)
 	else:
 		with open(path, 'r', encoding='utf-8') as fp:
 			basename = os.path.basename(path)
+			contents = parse_cmds(fp.read())
 			return render_template(
 				'base.jinja',
 				title=basename, 
-				body=fp.read(), 
+				body=contents, 
 				client_ip=remote_addr(),
 				url_up=url_up(),
 				)
